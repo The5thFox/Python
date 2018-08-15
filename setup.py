527d5ec20716fc4094e91f60a16faec3afe2be3c
The5thFox <The5thFox@users.noreply.github.com>
2018-08-15 12:41:46 -0400

    Created setup file
    Create a script to open the websites, programs, and file folders required to start python excercises

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import webbrowser
+
+webbrowser.open('https://github.com/The5thFox/Python')
+webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
+
+filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
+os.startfile(filepath1)
+filepath2 = r'"C:\Users\qtodd200\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook.lnk"'
+os.startfile(filepath2)
+filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
+os.startfile(filepath3)
