1f824a718705422b6baa7969c09e056f883bec89
The5thFox <The5thFox@users.noreply.github.com>
2018-08-15 23:11:00 -0400

    update to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if x == 'qtodd200':
     webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
     filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
     os.startfile(filepath1)
-    filepath2 = r'"C:\Users\Q\Jupyter Notebook.lnk"'
+    filepath2 = r'"C:\Users\qtodd200\Jupyter Notebook.lnk"'
     os.startfile(filepath2)
     filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
     os.startfile(filepath3)
