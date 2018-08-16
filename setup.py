2d88ca3a47b8116f60211e812865e1ed0c4ab748
The5thFox <The5thFox@users.noreply.github.com>
2018-08-15 21:45:12 -0400

    setup to open applications on a give pc

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 import os
+import win32api
 import webbrowser
 
-webbrowser.open('https://github.com/The5thFox/Python')
-webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
+os.environ['USERNAME']
+x = win32api.GetUserName()
 
-filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
-os.startfile(filepath1)
-filepath2 = r'"C:\Users\qtodd200\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook.lnk"'
-os.startfile(filepath2)
-filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
-os.startfile(filepath3)
+if x == 'qtodd200':
+    webbrowser.open('https://github.com/The5thFox/Python')
+    webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
+    filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
+    os.startfile(filepath1)
+    filepath2 = r'"C:\Users\qtodd200\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook.lnk"'
+    os.startfile(filepath2)
+    filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
+    os.startfile(filepath3)
+else:
+    webbrowser.open('https://github.com/The5thFox/Python')
+    webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
+    filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
+    os.startfile(filepath1)
+    filepath2 = r'"C:\Users\qtodd200\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook.lnk"'
+    os.startfile(filepath2)
+    filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
+    os.startfile(filepath3)
