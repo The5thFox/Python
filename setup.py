3c356a6c84510269d37791cb20550f64e04e5b58
The5thFox <The5thFox@users.noreply.github.com>
2018-08-15 22:56:41 -0400

    updated file paths

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ if x == 'qtodd200':
     webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
     filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
     os.startfile(filepath1)
-    filepath2 = r'"C:\Users\qtodd200\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook.lnk"'
+    filepath2 = r'"C:\Users\Q\Jupyter Notebook.lnk"'
     os.startfile(filepath2)
     filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
     os.startfile(filepath3)
 else:
     webbrowser.open('https://github.com/The5thFox/Python')
     webbrowser.open('https://www.udemy.com/complete-python-bootcamp/learn/')
-    filepath1 = r'"C:\Users\qtodd200\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
+    filepath1 = r'"C:\Users\Q\AppData\Local\GitHubDesktop\GitHubDesktop.exe"'
     os.startfile(filepath1)
-    filepath2 = r'"C:\Users\qtodd200\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook.lnk"'
+    filepath2 = r'"C:\Users\Q\Jupyter Notebook.lnk"'
     os.startfile(filepath2)
-    filepath3 = r'"C:\Users\qtodd200\workbooks\Python Example Code\Python"'
+    filepath3 = r'"C:\Users\Q\workbooks\Python Example Code\Python\"'
     os.startfile(filepath3)
